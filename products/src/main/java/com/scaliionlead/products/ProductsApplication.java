87f79d5391e27089cd6cdb71768b1fe5ec035e71
xlogger <xloggerster@gmail.com>
2020-04-26 03:12:35 +0800

    keycloak security adapter on products api

--- products/src/main/java/com/scaliionlead/products/ProductsApplication.java
+++ products/src/main/java/com/scaliionlead/products/ProductsApplication.java
@@ -1,9 +1,11 @@
 package com.scaliionlead.products;
 
 import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
+@EnableAutoConfiguration
 public class ProductsApplication {
 
 	public static void main(String[] args) {
